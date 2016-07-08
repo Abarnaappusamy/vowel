2e7663247d4251ab394fa91a49e0b59aeb2d79d9
Abarnaappusamy <abarnaappusamy@gmail.com>
2016-07-08 15:19:08 +0530

    finish

---
+++ vowel.c
@@ -0,0 +1,14 @@
+# vowel
+#include<stdio.h>
+void main()
+{
+char a;
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a='E'||a=='I'||a=='O'||a=='U')
+{
+printf("It is a vowel");
+}
+else
+{
+printf("It is consonant");
+}
+}
